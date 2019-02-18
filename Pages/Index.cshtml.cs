b1b5164cd9fb377ca3941958c46f45ca5a12dd0d
Юнак Андрей <kristozer@gmail.com>
2019-02-18 22:48:59 +1000

    добавил Onget

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -4,14 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using NewYearMusic.Data;
+using NewYearMusic.ViewModels;
 
 namespace NewYearMusic.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _context;
+        public IndexModel(ApplicationDbContext context)
         {
-
+            _context = context;
+        }
+        public IEnumerable<SongViewModel> OnGet()
+        {
+            return _context.Songs.Include(x=>x.User).AsNoTracking()
+            .Select(x=> new SongViewModel{SongId = x.SongId, Author = x.Author, Name = x.Name, User = x.User.UserName})
+            .ToList();
         }
     }
 }
---
+++ ViewModels/SongViewModel.cs
@@ -0,0 +1,10 @@
+namespace NewYearMusic.ViewModels
+{
+    public class SongViewModel
+    {
+        public int SongId { get; set; }
+        public string Author { get; set; }
+        public string Name { get; set; }
+        public string User { get; set; }
+    }
+}

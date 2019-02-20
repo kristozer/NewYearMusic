96714e1f115d62f3e4e0ed3c6f3b2bbfafefe570
Юнак Андрей <kristozer@gmail.com>
2019-02-20 22:59:01 +1000

    изменил ссылки после перемещения Data

--- Migrations/ApplicationDbContextModelSnapshot.cs
+++ Migrations/ApplicationDbContextModelSnapshot.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using NewYearMusic.Data;
+using NewYearMusic.Infrastructure.Data;
 
 namespace NewYearMusic.Migrations
 {
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using NewYearMusic.Data;
+using NewYearMusic.Infrastructure.Data;
 using NewYearMusic.ViewModels;
 
 namespace NewYearMusic.Pages

9624b87778c2d1ce2f9c6d50ec0fc53aaf4cb361
Юнак Андрей <kristall@empl.vvsu.ru>
2019-02-26 18:02:24 +1000

    добавил возможность фильтрации по id

--- Domain/Specifications/SongFilterSpecification.cs
+++ Domain/Specifications/SongFilterSpecification.cs
@@ -4,8 +4,9 @@ namespace NewYearMusic.Domain.Specifications
 {
     public sealed class SongFilterSpecification : BaseSpecification<Song>
     {
-        public SongFilterSpecification(string userName)
-        : base(i => string.IsNullOrEmpty(userName) || i.User.UserName == userName)
+        public SongFilterSpecification(string userName = null, int? id = null)
+        : base(i => (string.IsNullOrEmpty(userName) || i.User.UserName == userName) && 
+        (id == null || i.Id == id))
         {
             AddInclude(x => x.User);
         }
